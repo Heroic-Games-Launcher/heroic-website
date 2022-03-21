e9ae02722d101f194f9d1ef01e4ff9dc2a74b98c
Flavio F Lima <flavioislima@gmail.com>
2022-03-21 19:36:58 +0100

    Build: remove unused config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
-  reactStrictMode: true,
-  assetPrefix: isProd ? '/heroic-website/' : ''
+  reactStrictMode: true
 }
