5c56e4c1ff2e4f8ccb381109379d9b5ffc08d275
Flavio F Lima <flavioislima@gmail.com>
2022-03-22 16:02:20 +0100

    General Fixes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
 const isProd = process.env.NODE_ENV === 'production'
-module.exports = {
-  reactStrictMode: true
-}
+
+module.exports = withPlugins([
+  [optimizedImages, {
+    optipng: {
+      optimizationLevel: 4,
+    },
+  }], {
+    reactStrictMode: true,
+    images: {
+      disableStaticImages: true
+  }
+  }])
