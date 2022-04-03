34600ec5016ddd27e750a4809b59b62d62b62ffc
Flavio F Lima <flavioislima@gmail.com>
2022-04-03 13:45:47 +0200

    Revert "Image Optimizations with Next"
    This reverts commit f14fc9421692eafbff79efb96965f2f2637f40e6.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
 
 const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = withPlugins([withBundleAnalyzer], [
-  [optimizedImages], {
+module.exports = withPlugins([
+  [optimizedImages, {
+    optipng: {
+      optimizationLevel: 3,
+    },
+  }], {
     reactStrictMode: true,
     images: {
       disableStaticImages: true
