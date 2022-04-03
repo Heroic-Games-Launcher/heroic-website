54ceae75f1a3976bc271fb643a07f0c2defe5b77
Flavio F Lima <flavioislima@gmail.com>
2022-04-03 11:52:43 +0200

    Fix Home Page Link

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,35 +5,37 @@ const logo = require('../assets/logo.png')
 import { LogoGithub, LogoDiscord } from 'react-ionicons'
 
 export default function Header() {
-  return (
-    <nav className="container-fluid">
-    <ul>
-        <li>
-            <a href="/" className="contrast">
-                <img src={logo} alt="Heroic Games Launcher"
-                    style={{height: '32px'}} />
-                <strong>Heroic Games Launcher</strong>
-            </a>
-        </li>
-    </ul>
-    <ul>
-        <li>
-            <a href="https://github.com/Heroic-Games-Launcher">
-                <LogoGithub color={'#00000'} />
-            </a>
-        </li>
-        <li>
-            <a href="https://discord.com/invite/rHJ2uqdquK">
-                <LogoDiscord color={'#00000'} />
-            </a>
-        </li>
-        <li>
-            <a href="https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/wiki">Documentation</a>
-        </li>
-        <li>
-            <Link href="/donate">Support Us</Link>
-        </li>
-    </ul>
-</nav>
-  )
+    return (
+        <nav className="container-fluid">
+            <ul>
+                <li>
+                    <Link href="/">
+                        <span className="contrast link">
+                            <img src={logo} alt="Heroic Games Launcher"
+                                style={{ height: '32px' }} />
+                            <strong>Heroic Games Launcher</strong>
+                        </span>
+                    </Link>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href="https://github.com/Heroic-Games-Launcher">
+                        <LogoGithub color={'#00000'} />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://discord.com/invite/rHJ2uqdquK">
+                        <LogoDiscord color={'#00000'} />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/Heroic-Games-Launcher/HeroicGamesLauncher/wiki">Documentation</a>
+                </li>
+                <li>
+                    <Link href="/donate">Support Us</Link>
+                </li>
+            </ul>
+        </nav>
+    )
 }
