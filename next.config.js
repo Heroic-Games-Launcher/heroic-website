2a4517a052dd80717fad88af4371c133c7a9ee5d
Flavio F Lima <flavioislima@gmail.com>
2022-04-21 18:10:44 +0200

    [Config] Prettier and other stuff

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
-const isProd = process.env.NODE_ENV === 'production'
-
 module.exports = withPlugins([
   [optimizedImages, {
     optipng: {
