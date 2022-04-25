8effc1af17e8ff7dd021212b18e25a1cbc1894a0
Flavio F Lima <flavioislima@gmail.com>
2022-04-25 12:06:50 +0200

    [Fix] Get correct path on change

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,26 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useEffect } from 'react'
+import { useRouter } from 'next/router'
 import Footer from './Footer'
 import Navbar from './Navbar'
 import WebsiteHead from './WebsiteHead'
+import { pageview } from '../pages/api/ga'
 
 type Props = {
     children: ReactNode
 }
 
-export const Layout = ({children}: Props) => {
+export const Layout = ({ children }: Props) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+  
   return (
     <>
       <WebsiteHead />
