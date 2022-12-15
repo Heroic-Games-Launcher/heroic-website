7891ffebb5f92f5e575a0f16f3e36dff44416dd4
Flávio F Lima <flavioislima@gmail.com>
2022-12-15 14:48:37 +0100

    chore: updated mac instructions again

--- pages/downloads.tsx
+++ pages/downloads.tsx
@@ -221,8 +221,7 @@ const Downloads: NextPage = () => {
                   Might need to run this command on the terminal to make it
                   work:
                   <code>
-                    sudo xattr -r -d com.apple.quarantine | mdfind
-                    kMDItemCFBundleIdentifier = "*heroic"
+                    xattr -d com.apple.quarantine | mdfind kMDItemCFBundleIdentifier = "*heroic"
                   </code>
                 </p>
                 <footer>
