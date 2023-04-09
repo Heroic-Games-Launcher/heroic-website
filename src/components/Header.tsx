5000378105b032943a95b91ce666da8871853060
Ali Paredes <alinorberto168@gmail.com>
2023-04-09 15:02:48 -0400

    change header to client

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import logo from "../../public/images/logo.png";
 
 const pagePaths = [
@@ -22,6 +25,10 @@ const pagePaths = [
 ];
 
 export default function Header() {
+  const [open, setOpen] = useState(false);
+  const showMenu = () => {
+    setOpen(!open);
+  };
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -37,6 +44,7 @@ export default function Header() {
           </span>
         </Link>
         <button
+          onClick={showMenu}
           data-collapse-toggle="navbar-default"
           type="button"
           className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -52,13 +60,20 @@ export default function Header() {
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              fill-rule="evenodd"
+              fillRule="evenodd"
               d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clip-rule="evenodd"
+              clipRule="evenodd"
             ></path>
           </svg>
         </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+        <div
+          className={`${
+            open
+              ? "w-full md:block md:w-auto"
+              : "hidden w-full md:block md:w-auto"
+          }`}
+          id="navbar-default"
+        >
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             {pagePaths.map(({ label, url }) => (
               <li key={url}>
