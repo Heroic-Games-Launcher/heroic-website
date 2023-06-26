bd3639e1e3aad35449822942ae6e31d1bf12bacf
Ali Paredes <alinorberto168@gmail.com>
2023-06-26 14:30:41 -0400

    Add social icons

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,31 +1,25 @@
+import { DiscordIcon, GithubIcon, MastodonIcon, TwitterIcon } from './Icons'
 import { Logo } from './Navbar/Logo'
 
 const year = new Date().getFullYear()
 
 export const Footer = (): JSX.Element => {
   return (
-    <footer className="bg-white shadow dark:bg-gray-900">
+    <footer className="bg-white dark:bg-gray-900">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
           <Logo className="flex items-center mb-4 sm:mb-0" />
-          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+          <ul className="flex flex-wrap items-center gap-4 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
-              <a href="#" className="mr-4 hover:underline md:mr-6 ">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#" className="mr-4 hover:underline md:mr-6">
+              <a href="#" className="hover:underline">
                 Privacy Policy
               </a>
             </li>
             <li>
-              <a href="#" className="mr-4 hover:underline md:mr-6 ">
-                Licensing
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
+              <a
+                href="mailto:heroicgameslauncher@protonmail.com"
+                className="hover:underline"
+              >
                 Contact
               </a>
             </li>
@@ -33,11 +27,40 @@ export const Footer = (): JSX.Element => {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © {year}{' '}
-          <a href="#" className="hover:underline">
-            Heroic Games Launcher™
-          </a>
-          . All Rights Reserved.
+          © {year} Heroic Games Launcher™. All Rights Reserved.
+          <div className="flex sm:justify-center items-center gap-2 mt-4">
+            <a
+              href="https://github.com/Heroic-Games-Launcher"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GithubIcon className="w-5 h-5" />
+            </a>
+            <hr className="block h-4 w-[1px] dark:bg-slate-700 bg-slate-400 border-none" />
+            <a
+              href="https://discord.com/invite/rHJ2uqdquK"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <DiscordIcon className="w-5 h-5" />
+            </a>
+            <hr className="block h-4 w-[1px] dark:bg-slate-700 bg-slate-400 border-none" />
+            <a
+              href="https://twitter.com/HeroicLauncher"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <TwitterIcon className="w-5 h-5" />
+            </a>
+            <hr className="block h-4 w-[1px] dark:bg-slate-700 bg-slate-400 border-none" />
+            <a
+              href="https://mastodon.social/@heroiclauncher"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <MastodonIcon className="w-5 h-5" />
+            </a>
+          </div>
         </span>
       </div>
     </footer>
