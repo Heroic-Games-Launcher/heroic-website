106f5b4d31cb6597de5c806b08a1e85d7e05caf1
Flavio F Lima <flavioislima@users.noreply.github.com>
2025-03-26 17:32:36 +0000

    Revert "Add Plausible script to _document.tsx and create new utility files"
    This reverts commit 634c472b85804864123dd2083402599fe56df373.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <script defer data-domain="heroicgameslauncher.com" src="https://plausible.io/js/script.file-downloads.outbound-links.js"></script>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
