f19bae02a22dcbf231910b4704eb8761bbc7c50f
Flavio F Lima <flavioislima@users.noreply.github.com>
2025-03-26 17:33:25 +0000

    Add Plausible

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script defer data-domain="heroicgameslauncher.com" src="https://plausible.io/js/script.file-downloads.outbound-links.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
